0c9f0b7aec4329fa829b98373db218fc3084ba9e
yapcheahshen <yapcheahshen@gmail.com>
2017-02-24 14:17:47 +0800

    ready for accelon2017

--- gen.js
+++ gen.js
@@ -5,29 +5,42 @@ var files=fs.readFileSync("./file.lst","utf8").split(/\r?\n/);
 //for (var i=0;i<24;i++) files.shift();
 //files.length=5;
 
+var composes=["第零編"],categories=[], groupid;
+const capture=function(){return true;}
+const on_compose_close=function(tag,closing,kpos,tpos){
+	const compose=this.popText();
+	composes.push(compose);
+}
+
+const on_category_close=function(tag,closing,kpos,tpos){
+	const cat=this.popText();
+	this.putGroup(groupid+";"+(composes.length-1)+"@"+cat,kpos,tpos);
+}
+
 const bookStart=function(){}
 const bookEnd=function(){}
 const fileStart=function(fn,i){
 	const at=fn.lastIndexOf("/");
 	console.log(fn)
 	fn=fn.substr(at+1);
-	fn=fn.substr(0,fn.length-4);//remove .xml
-	var kpos=this.kPos;
-	if (this.kPos) kpos=this.nextLineStart(this.kPos); //this.kPos point to last char of previos file
-	this.putField("file",fn,kpos);
+	groupid=fn.substr(0,fn.length-4);//remove .xml
 }
+
 var options={name:"taixu",inputFormat:"accelon3",
 article:"文",subtoc:"文",
 bitPat:"taixu",
-autoStart:true,textOnly:true}; //set textOnly not to build inverted
+groupPrefix:composes,
+articleFields:["head","ptr","def","p"],
+title:"太虛大師全書",
+autoStart:true}; //set textOnly not to build inverted
 
 var corpus=createCorpus(options);
 corpus.setHandlers(
-	{},
-	{},
+	{"類":capture,"編":capture},
+	{"類":on_category_close,"編":on_compose_close},
 	{bookStart,bookEnd,fileStart}  
 );
-
+console.time("build")
 files.forEach(fn=>corpus.addFile(sourcepath+fn));
 
 corpus.writeKDB("taixu.cor",function(byteswritten){
@@ -36,5 +49,5 @@ corpus.writeKDB("taixu.cor",function(byteswritten){
 
 //console.log("Max Article Length",maxArticle())
 
-
+console.timeEnd("build")
 console.log(corpus.totalPosting,corpus.tPos);
